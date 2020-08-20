105792fe4cd3d1752dead988309acd7e3c5a5908
maxihac <cmaxihac@gmail.com>
2020-08-20 11:53:14 -0300

    nueva version del ej2

--- tema-5/ejercicio2.js
+++ tema-5/ejercicio2.js
@@ -1,4 +1,4 @@
-let titulo="tabla clima"; /*como poner un script dentro del codigo html, como un style en CSS*/
+let titulo = "tabla clima"; /*como poner un script dentro del codigo html, como un style en CSS*/
 console.log(titulo);
 let tabla_clima = [{
     mes: "Enero",
@@ -38,26 +38,18 @@ let tabla_clima = [{
   }
 ]
 
-function tempmayor(tabla) {
+function promedios(tabla) {
   let mayor_temp = 0;
-  for (elemento of tabla)
-    mayor_temp = mayor_temp + elemento.temp_max_abs;
-  return mayor_temp / tabla.length;
-}
+  let result=[0, 0, 0];
+  for (var i = 0; i < tabla.length; i++) {
+    result[0] = result[0] + tabla[i].temp_max_abs;
+    result[1] = result[1] + tabla[i].temp_min_abs;
+    result[2] = result[2] + tabla[i].humedad_rel;
+  }
+  result[0] = result[0] / tabla.length;
+  result[1] = result[1] / tabla.length;
+  result[2] = result[2] / tabla.length;
 
-function tempmenor(tabla) {
-  let menor_temp = 0;
-  for (elemento of tabla)
-    menor_temp = menor_temp + elemento.temp_min_abs;
-  return menor_temp / tabla.length;
+return result;
 }
-
-function humedad(tabla) {
-  let humedad_r = 0;
-  for (elemento of tabla)
-    humedad_r = humedad_r + elemento.humedad_rel;
-  return humedad_r / tabla.length;
-}
-console.log("La mayor temperatura promedio absoluta es", tempmayor(tabla_clima));
-console.log("La menor temperatura promedio absoluta es", tempmenor(tabla_clima));
-console.log("La humedad promedio es", humedad(tabla_clima));
+  console.log("La mayor y menor temperatura y humedad  promedio son", promedios(tabla_clima));
